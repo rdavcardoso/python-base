7debfe57327c79f07b76b40cb8172a828e108019
Rogerio Cardoso <rogerio.davcardoso@gmail>
2024-04-02 10:46:53 -0300

    Script escola_v2_com_set.py refatorado no script escola_v3_com_dict.py utilizando dicionário

---
+++ escola_v3_com_dict.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Exibe relatório de crianças agrupadas por sala
+que frequentam cada uma das atividades.
+"""
+__version__ = "0.1.1"
+__author__ = "Rogerio"
+
+#Dados
+salas = {
+    "sala1": ["Erik", "Maia","Gustavo","Manuel", "Sofia", "Joana"],
+    "sala2": ["Joao", "Antonio", "Carlos", "Maria", "Isolda"],
+}
+
+atividades = {
+    "Inglês": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "Musica": ["Erik", "Carlos", "Maria"],
+    "Danca": ["Gustavo", "Sofia", "Joana", "Antonio"],
+}
+
+
+#Listar alunos em cada atividade por sala
+
+for nome_atividade, alunos_atividade in atividades.items():
+    for nome_sala, alunos_sala in salas.items():
+        print(f"{nome_atividade} {nome_sala}: {set(alunos_atividade) & set(alunos_sala)}")
+    print("-" * 45)    
+        
+    
+
+
+   
+    
