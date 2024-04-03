a2339021307ddd817728eac440865588f49d04a9
Rogerio Cardoso <rogerio.davcardoso@gmail>
2024-04-03 09:17:43 -0300

    Script atualizado utilizando dicionário

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Rogerio Cardoso"
 __license__ = "Unlincese"
 
@@ -24,16 +24,12 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5] 
 
-msg = "Hello, World!"
-
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT": 
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP": 
-    msg = "Holla, Mundo!"
-elif current_language == "fr_FR": 
-    msg = "Bonjour, Monde!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+} 
     
-
-print(msg)
+print(msg[current_language])
