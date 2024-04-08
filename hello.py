94f375da53e5f107e7aaceceda8bd1c9fa59ca1b
Rogerio Cardoso <rogerio.davcardoso@gmail>
2024-04-08 17:56:33 -0300

    Adicionando lógicas de input para evitar erros

--- hello.py
+++ hello.py
@@ -10,19 +10,47 @@ Tenha a variável LANG devidamente configurada, Ex:
 
     export LANG=pt_BR
 
+ou informe atraves do CLI argument `--lang`
+
+ou o usuário terá que digitar a linguagem que quer.
+
 Execução:
 
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Rogerio Cardoso"
 __license__ = "Unlincese"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5] 
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    
+    else:
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
+    
 
 msg = {
     "en_US": "Hello, World!",
@@ -32,4 +60,4 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 } 
     
-print(msg[current_language])
+print((msg[current_language] + "\n") * int(arguments["count"]))
