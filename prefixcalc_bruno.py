8d78fa87f537e673a4ace9ea2accd40a5491cc47
Rogerio Cardoso <rogerio.davcardoso@gmail>
2024-04-10 10:48:08 -0300

    Script feito com auxílio do professor

---
+++ prefixcalc_bruno.py
@@ -0,0 +1,67 @@
+"""Calculadora prefix
+
+Versão feita sem auxílio.
+
+Funcionamento:
+
+[operação] [n1] [n2]
+
+Operações:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ prefixcalc.py sum 5 2
+7
+
+$ prefixcalc.py mul 10 5
+50
+
+$ prefixcalc.py 
+operação: sum
+n1: 5
+n2: 4
+9
+"""
+__version__ = "0.1.0"
+
+import sys
+
+arguments = sys.argv[1:]
+
+if not arguments:
+    operation = input("Operação: ")
+    n1 = input("n1: ")
+    n2 = input("n2: ")
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print("Número de argumentos inválidos!")
+    print("Ex: `sum 5 5`")
+    sys.exit(1)
+
+operation, *nums = arguments
+validated_nums = []
+for num in nums:
+    if not num.replace(".", "").isdigit():
+        print(f"Numero inválido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+if operation == "sum":
+    result = n1 + n2
+elif operation == "sub":
+    result = n1 - n2
+elif operation == "mul":
+    result = n1 * n2
+elif operation == "div":
+    result = n1 / n2
+
+print(f"O resultado é {result}")
